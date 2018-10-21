242c009fa8c36a8df2827de74b59f1922814e00e
yaoandy107 <yaoandy107@gmail.com>
2018-10-22 02:54:48 +0800

    Integrate header components

---
+++ components/public/header/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row 
+    :gutter="15" 
+    class="m-header">
+    <el-col :span="4"><department/></el-col>
+    <el-col :span="3"><user/></el-col>
+    <el-col :span="17"><navbar/></el-col>
+  </el-row>
+</template>
+
+<script>
+import Department from './department.vue'
+import User from './user.vue'
+import Navbar from './nav.vue'
+export default {
+  components: {
+    Department,
+    User,
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+a {
+  color: #999;
+}
+</style>
