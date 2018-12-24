47b7ddc7e10bf8145a89253002c8a69b295b5eca
PrismRealm <alan159753@gmail.com>
2018-12-24 18:01:40 +0800

    Change the validator of 'email' field to email()

--- server/models/Member.js
+++ server/models/Member.js
@@ -29,7 +29,7 @@ const PROFILE_VALIDATOR = Joi.object().keys({
     .regex(/^\d{10,10}$/)
     .required(),
   email: Joi.string()
-    .regex(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    .email()
     .required()
 })
 
