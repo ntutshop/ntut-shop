94a9a5111a22c5b435004b753ff25015a62da37a
PrismRealm <alan159753@gmail.com>
2018-12-25 23:12:01 +0800

    Change config settings

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,8 @@ export let SERVER_CONFIG = {
   HOST: '127.0.0.1',
   PORT: 3000,
   JWT_SECRET: 'mysecret',
-  MODE: 'development'
+  MODE: process.env.NODE_ENV,
+  BASE_URL: `https://${DOMAIN}`
 }
 
 export let DB_CONFIG = {
@@ -19,5 +20,5 @@ export let FACEBOOK_CONFIG = {
   APP_ID: '444077546126314',
   APP_SECRET: '7676f48d1ae95328f3c4d99a1d1c9df5',
   CLIENT_TOKEN: '6452583eb0b6665e2f7e3a741a7e095f',
-  REDIRECT_URI: `https://${DOMAIN}/oauth/facebook/callback`
+  REDIRECT_URI: `https://${DOMAIN}/api/oauth/facebook/callback`
 }
