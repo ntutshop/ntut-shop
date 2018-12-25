ab0065738fb314dc3ddd4e2ed434b02a18a2a115
PrismRealm <alan159753@gmail.com>
2018-12-26 01:54:16 +0800

    Fix 'on-development' behavior

--- server/routes/dev.js
+++ server/routes/dev.js
@@ -1,8 +1,11 @@
 import KoaRouter from 'koa-router'
 import controller from '../controllers/dev.js'
+import { SERVER_CONFIG } from '../config/config.js'
 
 const router = new KoaRouter({ prefix: '/dev' })
 
-router.get('/facebook_login', controller.FakeFacebookLogin)
+if (SERVER_CONFIG.MODE !== 'production') {
+  router.get('/facebook_login', controller.FakeFacebookLogin)
+}
 
 export default router
