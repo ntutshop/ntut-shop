6ce8dade5d57dde6b19c067376434d4ea9d048e0
DevilTea <ch870814@gmail.com>
2018-12-25 09:30:28 +0800

    add textfields and a button

---
+++ pages/signup.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-layout
+      row
+      wrap
+      justify-center
+    >
+      <v-card
+        width="500"
+        class="pa-4">
+        <v-card-title
+          class="headline"
+          primary-title>
+          完成個人資訊填寫
+        </v-card-title>
+        <v-card-text>
+          <v-layout
+            row
+            wrap>
+            <v-flex xs12>
+              <v-text-field
+                id="username"
+                name="username"
+                label="使用者名稱"
+                color="blue"
+              />
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                id="nickname"
+                name="nickname"
+                label="暱稱"
+                color="blue"
+              />
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                id="phone"
+                name="phone"
+                label="手機號碼"
+                color="blue"
+              />
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                id="email"
+                name="email"
+                label="信箱"
+                color="blue"
+              />
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn
+            color="blue"
+            flat>送出</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
