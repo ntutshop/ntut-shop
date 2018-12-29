11e651bedba63fbee7dcc291895692bffb9a2ee1
PrismRealm <alan159753@gmail.com>
2018-12-29 21:47:24 +0800

    Attach user router to main router.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,14 @@
 import KoaRouter from 'koa-router'
 
 import oauthRouter from './authorization.js'
+import userRouter from './user.js'
 
 const router = new KoaRouter({ prefix: '/api' })
 
 // Routes for authorization.
 router.use(oauthRouter.routes()).use(oauthRouter.allowedMethods())
+
+// Routes for users' data.
+router.use(userRouter.routes()).use(userRouter.allowedMethods())
 
 export default router
