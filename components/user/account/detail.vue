150d4da73bb74fbad329f1e8847c1569b84dce49
DevilTea <ch870814@gmail.com>
2019-01-01 05:02:52 +0800

    add real function

--- components/user/account/detail.vue
+++ components/user/account/detail.vue
@@ -9,69 +9,141 @@
       label-width="100px"
     >
       <el-form-item
+        :inline-message="true"
+        :error="error.username"
         label="使用者名稱"
         style="width: 400px;"
       >
         <el-input
           v-model="form.username"
           placeholder="請輸入使用者名稱"
+          @input="modified = true"
         />
       </el-form-item>
       <el-form-item
+        :inline-message="true"
+        :error="error.nickname"
         label="暱稱"
         style="width: 400px;"
       >
         <el-input
           v-model="form.nickname"
           placeholder="請輸入暱稱"
+          @input="modified = true"
         />
       </el-form-item>
       <el-form-item
+        :inline-message="true"
+        :error="error.phone"
         label="聯絡電話"
         style="width: 400px;"
       >
         <el-input
           v-model="form.phone"
           placeholder="請輸入聯絡電話"
+          @input="modified = true"
         />
       </el-form-item>
       <el-form-item
+        :inline-message="true"
+        :error="error.email"
         label="信箱"
         style="width: 400px;"
       >
         <el-input
           v-model="form.email"
           placeholder="請輸入信箱"
+          @input="modified = true"
         />
       </el-form-item>
       <el-form-item label="註冊日期">
-        <div>2018-10-11</div>
+        <div>{{ registerDate }}</div>
       </el-form-item>
       <el-form-item>
         <el-button
+          :disabled="!modified"
           type="primary"
-          @click="onSubmit"
+          @click="submit"
         >儲存</el-button>
-        <el-button>取消</el-button>
+        <el-button @click="reset">取消</el-button>
       </el-form-item>
     </el-form>
   </el-card>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   data() {
     return {
+      modified: false,
+      defaultData: () => ({
+        username: this.userInfo.username,
+        nickname: this.userInfo.nickname,
+        phone: this.userInfo.phone,
+        email: this.userInfo.email
+      }),
       form: {
-        username: 'deviltea',
-        nickname: '威任',
-        phone: '0970517333',
-        email: 'deviltea@gmail.com'
+        username: '',
+        nickname: '',
+        phone: '',
+        email: ''
+      },
+      error: {
+        username: '',
+        nickname: '',
+        phone: '',
+        email: ''
       }
     }
   },
+  computed: {
+    registerDate() {
+      if (this.userInfo.register_time) {
+        return new Date(
+          Date.parse('2018-12-31T09:59:43.000Z')
+        ).toLocaleDateString()
+      }
+      return '錯誤'
+    },
+    ...mapState(['userInfo'])
+  },
+  mounted() {
+    this.form = this.defaultData()
+  },
   methods: {
-    onSubmit() {}
+    async submit() {
+      try {
+        Object.keys(this.error).forEach(key => {
+          this.error[key] = ''
+        })
+        await this.$axios.put('/user/information', this.form)
+        await this.$store.dispatch('updateUserState')
+        this.reset()
+      } catch (e) {
+        const code = parseInt(e.response && e.response.status)
+        if (code === 400) {
+          Object.keys(this.error).forEach(key => {
+            this.error[key] = e.response.data.error[key]
+          })
+        } else if (code === 401) {
+          this.$store.dispatch('updateUserState')
+          this.$nuxt.error({ statusCode: 401 })
+        } else if (code === 403 && e.response.data.reason === 'unregistered') {
+          this.$store.dispatch('updateUserState')
+          this.$nuxt.error({ statusCode: 403, message: 'unregistered' })
+        } else {
+          throw e
+        }
+      }
+    },
+    reset() {
+      this.form = this.defaultData()
+      this.modified = false
+      Object.keys(this.error).forEach(key => {
+        this.error[key] = ''
+      })
+    }
   }
 }
 </script>
