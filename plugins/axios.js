dbc4d3e98f680543ba984ef08af13e74b308b0d0
DevilTea <ch870814@gmail.com>
2018-12-31 12:55:46 +0800

    add onerror handling

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,14 @@
 export default function ({ $axios, error }) {
-  $axios.onError(e => {
-    if (e.code === 401) {
-      error('OOPS！您尚未登入')
+  $axios.onError(async e => {
+    const code = parseInt(e.response && e.response.status)
+    if (code === 401) {
+      error({ statusCode: 401 })
+    } else if (code === 403 && e.response.data.reason === 'unregistered') {
+      error({ statusCode: 403, reason: 'unregistered' })
+    } else if (code === 403 && e.response.data.reason === 'permission-denied') {
+      error({ statusCode: 403, reason: 'permission-denied' })
+    } else {
+      throw e
     }
   })
 }
