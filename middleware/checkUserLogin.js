d114f303565736d326f1901dc3cd9d036560a6af
DevilTea <ch870814@gmail.com>
2018-12-31 20:01:01 +0800

    update error handling

--- middleware/checkUserLogin.js
+++ middleware/checkUserLogin.js
@@ -1,7 +1,14 @@
-export default async function ({ $axios }) {
+export default async function ({ $axios, route, store, error }) {
   try {
     await $axios.get('/user/state/login')
   } catch (e) {
-    console.log(e)
+    const code = parseInt(e.response && e.response.status)
+    if (code === 401) {
+      store.dispatch('setLoggedIn', false)
+      if (route.name !== 'index') error({ statusCode: 401 })
+    } else {
+      console.log('login error')
+      throw e
+    }
   }
 }
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,17 +1,2 @@
-export default function ({ $axios, error, store }) {
-  $axios.onResponseError(async e => {
-    const code = parseInt(e.response && e.response.status)
-    if (code === 401) {
-      store.dispatch('setLoggedIn', false)
-      error({ statusCode: 401 })
-    } else if (code === 403 && e.response.data.reason === 'unregistered') {
-      error({ statusCode: 403, message: 'unregistered' })
-    } else if (code === 403 && e.response.data.reason === 'permission-denied') {
-      error({ statusCode: 403, message: 'permission-denied' })
-    } else if (code === 403 && e.response.data.reason === 'registered') {
-      error({ statusCode: 403, message: 'registered' })
-    } else {
-      throw e
-    }
-  })
+export default function ({ $axios, error, store, route }) {
 }
